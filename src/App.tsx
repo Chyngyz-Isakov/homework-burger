4397586b72071efb055bca96129c018a268c5313
Chyngyz Isakov <ellegantus@gmail.com>
2023-05-30 16:09:15 +0600

    Download images for Ingredients and style css, also create INGREDIENTS name, price, img

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import meatImage from './assets/bacon.png';
+import baconImage from './assets/bacon.png';
+import cheeseImage from './assets/bacon.png';
+import saladImage from './assets/bacon.png';
 import './App.css';
+import {Ingredients} from "./type";
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
-}
+const App = () => {
+
+    const INGREDIENTS: Ingredients[] = [
+        {name: 'Meat', price: 80, image: meatImage},
+        {name: 'Bacon', price: 60, image: baconImage},
+        {name: 'Cheese', price: 50, image: cheeseImage},
+        {name: 'Salad', price: 10, image: saladImage},
+    ];
+
+
+  return(
+      <div className="App">
+
+      </div>
+  )
+};
 
 export default App;
---
+++ src/type.d.ts
@@ -0,0 +1,5 @@
+export type Ingredients = {
+    name: string;
+    price: number;
+    image: img;
+}
